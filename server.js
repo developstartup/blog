55eb1a7c879408451af7d685bdb9249c30dcd843
kim <kim@gmail.com>
2020-01-13 19:43:23 +0900

    express router

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+const product_route = require('./api/routes/products');
+const order_route = require('./api/routes/order');
+
+app.use('/product', product_route);
+app.use('/order', order_route);
+
+
 const PORT = 3000;
 
 app.listen(PORT, console.log("server started"));
