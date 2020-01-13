9bf1af7a875b58f16e91ef2823f7160d0ee19d1f
kim <kim@gmail.com>
2020-01-13 20:17:18 +0900

    add mongoose

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
 
 const product_route = require('./api/routes/products');
 const order_route = require('./api/routes/order');
+
+
+// const mongoDB_URL = "mongodb+srv://kim:123123123@cluster0-37bhl.mongodb.net/test?retryWrites=true&w=majority";
+const mongoDB_URL = "mongodb://teddykwak:k9915402@ds141294.mlab.com:41294/node-rest-shop";
+
+// Database connect
+mongoose.connect(mongoDB_URL, 
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }
+    )
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log(err));
+
+
+
+
 
 app.use('/product', product_route);
 app.use('/order', order_route);
