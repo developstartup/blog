05c784fb338bcb46b88e20d5c1ec140b476fbe50
kim <kim@gmail.com>
2020-01-13 20:00:23 +0900

    product & order CRUD

--- api/routes/order.js
+++ api/routes/order.js
@@ -2,7 +2,27 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    console.log("order get");
+    res.status(200).json({
+        msg: "order success"
+    });
+});
+
+router.post("/", (req, res) => {
+    res.status(200).json({
+        msg: "order post success"
+    });
+});
+
+router.patch("/", (req, res) => {
+    res.status(200).json({
+        msg: "order patch success"
+    });
+});
+
+router.delete("/", (req, res) => {
+    res.status(200).json({
+        msg: "order delete success"
+    });
 });
 
 module.exports = router;
--- api/routes/products.js
+++ api/routes/products.js
@@ -2,7 +2,27 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    console.log("product get");
+    res.status(200).json({
+        msg: "product success"
+    });
+});
+
+router.post("/", (req, res) => {
+    res.status(200).json({
+        msg: "product post success"
+    });
+});
+
+router.patch("/", (req, res) => {
+    res.status(200).json({
+        msg: "product patch success"
+    });
+});
+
+router.delete("/", (req, res) => {
+    res.status(200).json({
+        msg: "product delete success"
+    });
 });
 
 module.exports = router;
